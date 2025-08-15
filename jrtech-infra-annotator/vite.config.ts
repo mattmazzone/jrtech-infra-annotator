53b0067c03b339856c3633b2c1008fb5efd0f88c
Matteo Mazzone <imatteomazzone@gmail.com>
2025-08-15 00:01:43 -0400

    Update vite.config.ts

--- jrtech-infra-annotator/vite.config.ts
+++ jrtech-infra-annotator/vite.config.ts
@@ -3,10 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: '/jrtech-infra-annotator/', // 👈 This fixes GitHub Pages subpath 404
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(new URL('.', import.meta.url).pathname, './src'),
     },
   },
 })
