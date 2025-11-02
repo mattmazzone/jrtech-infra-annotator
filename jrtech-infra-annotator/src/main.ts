57b1a3998a6b2a3deb726f4eadf2d72896269df6
Matteo Mazzone <73369339+mattmazzone@users.noreply.github.com>
2025-11-01 23:40:16 -0400

    Add auth0 to app (#9)

--- jrtech-infra-annotator/src/main.ts
+++ jrtech-infra-annotator/src/main.ts
@@ -1,5 +1,6 @@
-import { createApp } from 'vue'
+import { createApp, watch } from 'vue'
 import { createPinia } from 'pinia'
+import { createAuth0 } from '@auth0/auth0-vue'
 import './index.css'
 
 import App from './App.vue'
@@ -7,6 +8,17 @@ import router from './router'
 
 const app = createApp(App)
 
+const auth0 = createAuth0({
+  domain: 'dev-ighikkkuy7psftc7.us.auth0.com',
+  clientId: 'KTTYtiTjU78ca3BNgUCvY8VOZDVoD5vf',
+  authorizationParams: {
+    redirect_uri: window.location.origin,
+  },
+  useRefreshTokens: true, // Enable refresh tokens
+  cacheLocation: 'localstorage', // Persist auth state across refreshes
+})
+
+app.use(auth0)
 app.use(createPinia())
 app.use(router)
 
